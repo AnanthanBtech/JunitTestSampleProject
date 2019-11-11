4f2fbcb45f2257d0ee751e9aa0bb7e7d9371b9f4
ananthan <anan.intech@gmail.com>
2019-11-11 19:33:19 +0530

    Update LeapYearTestCase.java

--- src/test/java/com/inc/user/utility/LeapYearTestCase.java
+++ src/test/java/com/inc/user/utility/LeapYearTestCase.java
@@ -14,28 +14,28 @@ import org.junit.Test;
 public class LeapYearTestCase {
 
 	@Test
-	public void leapTest0() {
+	public void leapTestPassBy2000() {
 		String msg = "Year 2000 should be a leap year.";
 		boolean result = LeapYear.checkYear(2000);
 		assertEquals(msg, true, result);
 	}
 
 	@Test
-	public void leapTest1() {
+	public void leapTestPassBy2004() {
 		String msg = "Year 2004 should be a leap year.";
 		boolean result = LeapYear.checkYear(2004);
 		assertEquals(msg, true, result);
 	}
 
 	@Test
-	public void leapTest2() {
+	public void leapTesFailsBy1900() {
 		String msg = "Year 1900 should not be a leap year.";
 		boolean result = LeapYear.checkYear(1900);
 		assertEquals(msg, false, result);
 	}
 
 	@Test
-	public void leapTest3() {
+	public void leapTestFailBy2005() {
 		String msg = "Year 2005 should not be a leap year.";
 		boolean result = LeapYear.checkYear(2005);
 		assertEquals(msg, false, result);
